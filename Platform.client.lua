376aeae8c77391b8651c7792921f3d414fcf8b98
jcook0 <52017130+jcook0@users.noreply.github.com>
2021-12-15 20:55:50 -0500

    Update Platform.client.lua

--- Platform.client.lua
+++ Platform.client.lua
@@ -3,13 +3,19 @@ local UserInputService = game:GetService("UserInputService")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local lp = game.Players.LocalPlayer
+local camera = workspace:WaitForChild("CurrentCamera")
 local event = ReplicatedStorage:WaitForChild("PlayerPlatform", 20)
 
 local function getPlatform()
 	if GuiService:IsTenFootInterface() then
 		return "Console"
 	elseif UserInputService.TouchEnabled and not UserInputService.MouseEnabled then
-		return "Mobile"
+        	local DeviceSize = camera.ViewportSize
+		if ( DeviceSize.Y > 600 ) then
+			return "Tablet"
+		else
+			return "Phone"
+		end
 	else
 		return "Desktop"
 	end
