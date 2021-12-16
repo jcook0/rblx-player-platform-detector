fd5ef208a5690babaae9429ed41f43f17355cda3
jcook0 <52017130+jcook0@users.noreply.github.com>
2021-12-15 20:12:38 -0500

    Create PlayerPlatform.lua

---
+++ PlayerPlatform.lua
@@ -0,0 +1,27 @@
+--[[ Services ]]--
+local StarterPlayer = game:GetService("StarterPlayer")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+--[[ Variables ]]--
+local starterPlayerScripts = StarterPlayer:FindFirstChild("StarterPlayerScripts")
+
+if not starterPlayerScripts then
+	starterPlayerScripts = Instance.new("StarterPlayerScripts", StarterPlayer)
+end
+
+local event = Instance.new("RemoteEvent", ReplicatedStorage)
+event.Name = "PlayerPlatform"
+
+local client = script.Platform
+client.Parent = starterPlayerScripts
+client.Disabled = false
+
+--[[ Server Event ]] --
+local eventConnection = event.OnServerEvent:Connect(function(player, platformType)
+	player:SetAttribute("Platform","Desktop")
+	
+	if not platformType then return end
+	
+	player:SetAttribute("Platform", platformType)
+end)
